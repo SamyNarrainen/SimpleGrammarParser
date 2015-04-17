f8b547c61268c1eee0c3e19227df6f7ecf1869dc
Samy Narrainen <samynarrainen@gmail.com>
2015-04-17 17:28:43 +0100

    Few additional functions, cleaning of previous ones
    Potential program errors produced by calling head, changed to cons
    function.
    
    Then a few new functions...

--- Haskell/Stack.hs
+++ Haskell/Stack.hs
@@ -5,10 +5,26 @@
 
 module Stack where 
 import Char
+import Grammar
+
+type Stack = [String]
 
 push :: a -> [a] -> [a]
 push a stack = a : stack 
 
 pop :: [a] -> (a, [a])
 pop []    = error "Out of bounds: the stack is empty."
-pop stack = (head stack, tail stack)
+pop ((:) x xs) = (x, xs)
+
+isEmpty :: [a] -> Bool
+isEmpty []    = True
+isEmpty stack = False
+
+pushRule :: Rule -> Stack -> Stack
+pushRule [] stack     = stack
+pushRule (x:xs) stack = pushRule xs (push x stack) 
+
+
+--pushRule (reverse (head (findRules "ta" (getRules (getPRule "nS" pRules1))))) (push "" [])
+
+--Main> pushRule (head (findRules "ta" (getRules (getPRule "nS" pRules1))))
